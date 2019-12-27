c5cdcad1cf165f0cedf469b32b6c0fe69dbb91e9
lab314-21@labeng.local <lab314-21@labeng.local>
2019-12-27 15:39:47 +0700

    1st

---
+++ lab3_3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int main ()
+{
+    float x,y,z;
+    x = 1 ;
+    y=2;
+    z=0;
+    while ( y<= 69)
+    {
+        z=z+(x/y);
+        y=y+1;
+    }
+    std :: cout << z;
+    
+}
